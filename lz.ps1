035a8f1015efcdfc9b20ccd507ac448be50b9867
Merrick Zhang <anphorea@gmail.com>
2024-03-25 16:41:19 +0800

    Update lz.ps1
    -Proxy removed.
    *URI changed according to site

--- lz.ps1
+++ lz.ps1
@@ -1,6 +1,5 @@
 Write-Host "Paste leakedzone.com profile page (ex. https://leakedzone.com/sweetiefox_of) and press enter"
 
-$Proxy = 'http://127.0.0.1:1081'
 $Url = Read-Host
 $Name = ([uri]$Url).Segments[-1].Trim('/')
 
@@ -9,31 +8,31 @@ New-Item -ItemType Directory -Force -Path ($Name + "\Videos\")
 
 $headers = @{
 'Accept-Encoding' = 'gzip, deflate'
-'Cookie' = 'XSRF-TOKEN=PASTE_HERE;laravel_session=PASTE_HERE; __cf_bm=PASTE_HERE'
+'Cookie' = 'XSRF-TOKEN=CHANGETHIS;cf_clearance=CHANGETHIS; leakedzone_session=CHANGETHIS'
 'X-Requested-With' = 'XMLHttpRequest'
 }
 
 $i = 0
 While ($True) {
-    $x = Invoke-WebRequest "$($Url)?page=$($i)&type=photos&order=0" -headers $headers -Proxy $Proxy
+    $x = Invoke-WebRequest "$($Url)?page=$($i)&type=photos&order=0" -headers $headers 
     $i++
     $A = ($x.content | ConvertFrom-Json)
     if (-not $A) { break }
     
     foreach ($item in $A) {
-        $photourl = "https://img.camhdxx.com/storage/" + $item.image
+        $photourl = "https://img.leakedzone.com/storage/" + $item.image
         if (-not $photourl) { continue }
         
         $filename = $Name + "\Photos\" + ([uri]$photourl).Segments[-1]
         if (-not (Test-Path $filename -PathType Leaf)) {
-            Invoke-WebRequest -Uri $photourl -Outfile $filename -Proxy $Proxy
+            Invoke-WebRequest -Uri $photourl -Outfile $filename
         }
     }
 }
 
 $i = 0
 While ($True) {
-    $x = Invoke-WebRequest "$($Url)?page=$($i)&type=videos&order=0" -headers $headers -Proxy $Proxy
+    $x = Invoke-WebRequest "$($Url)?page=$($i)&type=videos&order=0" -headers $headers
     $i++
     $A = ($x.content | ConvertFrom-Json)
     if (-not $A) { break }
@@ -47,6 +46,6 @@ While ($True) {
         $videourl = $videourl[-1..-$videourl.Length] -join ''
         $videourl = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($videourl))
         write-Host $videourl
-        yt-dlp $videourl -o "$Name\Videos\$slug.%(ext)s" --proxy $Proxy
+        yt-dlp $videourl -o "$Name\Videos\$slug.%(ext)s" 
     }
 }
